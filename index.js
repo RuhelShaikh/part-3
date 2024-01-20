4ec2d20c150bd9ec1afa1c43b6a86907b4baf075
RuhelShaikh <ruhelshaikh786@gmail.com>
2024-01-20 21:44:08 +0530

    render attempt 1

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
 
+app.use(cors());
+
 morgan.token("postData", (req) => {
   if (req.method === "POST") {
     return JSON.stringify(req.body);
@@ -20,7 +22,7 @@ app.use(
   })
 );
 app.use(express.json());
-app.use(cors());
+
 app.use(express.static("dist"));
 
 let persons = [
